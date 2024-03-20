6cc806ca31b5d2a7b74a9e8fc483129416bff66b
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-03-20 19:49:49 +0700

    Init Lab10

---
+++ src/Lab10/app/src/main/java/com/example/lab10/models/Note.java
@@ -0,0 +1,16 @@
+package com.example.lab10.models;
+
+public class Note {
+    private  int id;
+    private String title;
+    private  String content;
+
+    public Note(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+    public Note(){
+
+    }
+}
